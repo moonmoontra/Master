701cd27e6b0f531f5ef2ca5037c1370abe7d0836
Ksena <oksanaivaniv45@gmail.com>
2023-06-22 20:20:02 +0300

    New

--- persons/views.py
+++ persons/views.py
@@ -14,6 +14,12 @@ class ProviderListView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        # for provider in context['providersList']:
+        #     if provider.status == 'individual':
+        #         context['providersList'][provider].status = 'Фізична особа'
+        #     else:
+        #         context['providersList'][provider].status = 'Юридична особа'
         context['headers'] = ['№', 'Назва', 'Місто', 'Адреса', 'Телефон', 'Статус']
         context['fields'] = ['id', 'provider_name', 'city', 'address', 'phone', 'status']
+        #print(context['providersList'][0].status)
         return context
