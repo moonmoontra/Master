2a8b598c88b9fea94b06eb5b823229713998edd5
Ksena <oksanaivaniv45@gmail.com>
2023-06-23 13:16:07 +0300

    New

--- persons/views.py
+++ persons/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from persons.models import Provider, Manufacturer
+
+
 # Create your views here.
 
 def index(request):
@@ -10,7 +12,7 @@ def index(request):
 class ProviderListView(ListView):
     model = Provider
     template_name = 'provider_list.html'
-    context_object_name = 'providersList'
+    #context_object_name = 'providersList'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
