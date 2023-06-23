4e06bb502b1332ca1c99431240075e243ec52e09
lenivayavera <sherdakli33@gmail.com>
2023-06-24 01:43:52 +0300

    new

--- persons/services.py
+++ persons/services.py
@@ -1,9 +1,3 @@
-from django import forms
-from django.db import models
-
-from persons.models import Provider
-
-
 def get_model_context(model):
     context = {
         'headers': get_headers_table(model),
@@ -13,10 +7,6 @@ def get_model_context(model):
     return context
 
 
-def filter_objects_delete(objects, person_ids: list, **kwargs):
-    return objects.filter(id__in=person_ids, **kwargs).delete()
-
-
 def get_fields_table(model):
     excluded_fields = ['create_date', 'update_date']
     return [field.name for field in model._meta.get_fields() if field.name not in excluded_fields]
@@ -24,3 +14,7 @@ def get_fields_table(model):
 
 def get_headers_table(model):
     return [model._meta.get_field(field_name).verbose_name for field_name in get_fields_table(model)]
+
+
+def filter_objects_delete(objects, person_ids: list, **kwargs):
+    return objects.filter(id__in=person_ids, **kwargs).delete()
