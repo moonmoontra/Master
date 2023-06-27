41ac969402c41d0e2422e66981573513faac595f
Ksena <oksanaivaniv45@gmail.com>
2023-06-27 08:56:45 +0300

    New

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UnitOfMeasure, Price
+
+
+@admin.register(UnitOfMeasure)
+class UnitOfMeasureAdmin(admin.ModelAdmin):
+    list_filter = ('unit_name',)
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_filter = ('price_name',)
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseData(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True, verbose_name='Створено:')
+    update_date = models.DateTimeField(auto_now=True, verbose_name='Редаговано:')
+
+    class Meta:
+        abstract = True
+
+
+class UnitOfMeasure(BaseData):
+    unit_name = models.CharField(max_length=15, default=None, verbose_name='Назва')
+
+    def __str__(self):
+        return f"{self.unit_name}"
+
+    class Meta:
+        verbose_name = 'Одиниця вимірювання'
+        verbose_name_plural = 'Одиниці вимірювання'
+        ordering = ['unit_name']
+
+
+class Price(BaseData):
+    price_name = models.CharField(max_length=30, default=None, verbose_name='Назва')
+
+    def __str__(self):
+        return f"{self.price_name}"
+
+    class Meta:
+        verbose_name = 'Тип ціни'
+        verbose_name_plural = 'Типи цін'
+        ordering = ['price_name']
