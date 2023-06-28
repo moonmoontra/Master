0367e5be49cff1a05464383671397f6eddf4b11c
Ksena <oksanaivaniv45@gmail.com>
2023-06-28 14:09:32 +0300

    New

--- Master/settings.py
+++ Master/settings.py
@@ -83,14 +83,13 @@ WSGI_APPLICATION = 'Master.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'MasterDB'),
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'post4gr4sl5'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-        'PORT': os.environ.get('POSTGRES_PORT', ''),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
-#post4gr4sl5, RT%^&Ed25W//D
 
 
 # Password validation
