768b11c372c9e44613027c93b644e992fbad621b
lenivayavera <sherdakli33@gmail.com>
2023-07-06 01:59:41 +0300

    new

--- Master/settings.py
+++ Master/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fontawesomefree',
     'django_countries',
     'crispy_forms',
     'crispy_bootstrap5',
