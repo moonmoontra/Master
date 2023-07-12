4f02d01e3145d0a76091815437ea0a52d90fdb0d
lenivayavera <sherdakli33@gmail.com>
2023-07-12 17:38:16 +0300

    update

--- home/static/home/js/dark-theme-activation.js
+++ home/static/home/js/dark-theme-activation.js
@@ -1,3 +1,24 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var savedTheme = localStorage.getItem('theme');
+//     var mainNav = document.getElementById('main-nav');
+//
+//     if (savedTheme === 'dark') {
+//         document.body.classList.add('dark-theme');
+//         mainNav.setAttribute('data-bs-theme', 'dark');
+//     }
+// });
+//
+//
+// document.getElementById('theme-toggle').addEventListener('click', function () {
+//     console.log('clicked');
+//     document.body.classList.toggle('dark-theme');
+//     if (document.body.classList.contains('dark-theme')) {
+//         localStorage.setItem('theme', 'dark');
+//     } else {
+//         localStorage.setItem('theme', 'light');
+//     }
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
     var savedTheme = localStorage.getItem('theme');
     var mainNav = document.getElementById('main-nav');
@@ -8,12 +29,16 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+document.querySelector('.theme-toggle-dark').addEventListener('click', function () {
+    console.log('clicked');
+    document.body.classList.add('dark-theme');
+    document.body.classList.remove('light-theme');
+    localStorage.setItem('theme', 'dark');
+});
 
-document.getElementById('theme-toggle').addEventListener('click', function () {
-    document.body.classList.toggle('dark-theme');
-    if (document.body.classList.contains('dark-theme')) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
+document.querySelector('.theme-toggle-light').addEventListener('click', function () {
+    console.log('clicked');
+    document.body.classList.add('light-theme');
+    document.body.classList.remove('dark-theme');
+    localStorage.setItem('theme', 'light');
 });
