6ade1112ce3dc013658fbb6851b9c7ecb4832189
lenivayavera <sherdakli33@gmail.com>
2023-07-13 19:54:26 +0300

    update

--- home/templatetags/user_tags.py
+++ home/templatetags/user_tags.py
@@ -4,9 +4,12 @@ register = template.Library()
 
 
 @register.filter()
-# def get_attr(object, attr):
-#     return getattr(object, attr)
 def get_attr(obj, attr):
+    return getattr(obj, attr)
+
+
+@register.filter()
+def has_get_absolute_url(obj, attr):
     attr_value = getattr(obj, attr, None)
     if attr_value and hasattr(attr_value, 'get_absolute_url') and callable(attr_value.get_absolute_url):
         return attr_value.get_absolute_url()
